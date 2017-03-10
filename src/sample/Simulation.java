8844b8afe029e9ef94556e47ba3d90cd4a9552e9
Jacek Piłka <jacek001@wp.pl>
2017-03-10 17:59:23 +0100

    Proper simulation

--- src/sample/Simulation.java
+++ src/sample/Simulation.java
@@ -1,6 +1,11 @@
 package sample;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Random;
+
+import javax.swing.JFileChooser;
 
 public class Simulation {
 	
@@ -11,13 +16,15 @@ public class Simulation {
 	 * 2,3,4... - opinions
 	 */
 	
-	public int[] chain;
+	public static int[] chain;
+	public static int[] isolationsTable;
 	int opinions=0;
 	
 	Random rand = new Random();
 	
 	public Simulation(int chainSize, int numberOpinions) {
 		chain=new int[chainSize];
+		isolationsTable=new int[chainSize];
 		for(int i=0; i<chainSize; i++){
 			chain[i]=0;
 		}
@@ -47,13 +54,13 @@ public class Simulation {
 			//Sprawdzenie otoczenia - czy nastêpuje jakaœ izolacja
 			//Je¿eli opinia jest otoczona przez dwie inne (ale wzajemnie takie same), nastêpuje izolacja
 			if(chainSize-(n+1)>=2){
-				if(chain[n]==chain[n+2]&chain[n+1]!=chain[n]){
+				if(chain[n]==chain[n+2]&chain[n+1]!=chain[n]&chain[n+1]!=0){
 					chain[n+1]=1;
 					isolations++;
 				}
 			}
 			else if(chainSize-(n+1)<=chainSize-2){
-				if(chain[n]==chain[n-2]&chain[n-1]!=chain[n]){
+				if(chain[n]==chain[n-2]&chain[n-1]!=chain[n]&chain[n-1]!=0){
 					chain[n-1]=1;
 					isolations++;
 				}
@@ -64,12 +71,41 @@ public class Simulation {
 					isolations++;
 				}
 			}
-			for(int j=0; j<chainSize; j++)
-				System.out.print(chain[j]);
-			System.out.println();
+			isolationsTable[i]=isolations;
 		}
 	}
+	
+	public File fileChooser(){
+		JFileChooser chooser = new JFileChooser(); // Stworzenie klasy
+        chooser.setDialogTitle("Zapisywanie wyników"); // Ustawienie tytu³u okienka
+        int result = chooser.showDialog(null, "Zapisz"); //Otwarcie okienka. Metoda ta blokuje siê do czasu wybrania pliku lub zamkniêcia okna
+        if (JFileChooser.APPROVE_OPTION == result){ //Jeœli u¿ytkownik wybra³ plik
+        	return chooser.getSelectedFile();
+        }
+        else {
+            System.out.println("Nie wybrano pliku");
+            return null;
+        }
+	}	
+	public void save(){
+		try {
+			File plik = fileChooser();
+			PrintWriter out = new PrintWriter(plik);
+			out.println("Iteracja \t Izolacje");
+			for(int i=0; i<isolationsTable.length;i++){
+				out.print(i);
+				out.print("\t");
+				out.println(isolationsTable[i]);
+			}
+		    out.close();
+		} 
+			catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
-        Simulation simulacja = new Simulation(15,2);
+        Simulation symulacja = new Simulation(1000,2);
+        symulacja.save();
     }
 }
