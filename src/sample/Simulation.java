969ad436c221e9c5d35e085c39c1bdcf4c09e044
Jacek Piłka <jacek001@wp.pl>
2017-03-10 22:09:36 +0100

    N-chains simulation (not completed)
    Simulation works with N-chains, but it doesn't check every possibilities
    of isolation.

--- src/sample/Simulation.java
+++ src/sample/Simulation.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
-
 import javax.swing.JFileChooser;
 
 public class Simulation {
@@ -16,62 +15,138 @@ public class Simulation {
 	 * 2,3,4... - opinions
 	 */
 	
-	public static int[] chain;
+	public static int[][] chain;
 	public static int[] isolationsTable;
 	int opinions=0;
 	
 	Random rand = new Random();
 	
-	public Simulation(int chainSize, int numberOpinions) {
-		chain=new int[chainSize];
-		isolationsTable=new int[chainSize];
+	public Simulation(int chainSize, int numberOpinions, int numberChains) {
+		chain=new int[chainSize][numberChains];
+		isolationsTable=new int[chainSize*numberChains];
 		for(int i=0; i<chainSize; i++){
-			chain[i]=0;
+			for(int j=0; j<numberChains; j++){
+				chain[i][j]=0;
+			}
 		}
 		opinions=numberOpinions;
 		int isolations=0;
 		int numberTry=0; //Liczba prób znalezienia pustego elementu ³añcucha.
 						 //Je¿eli prób bêdzie za du¿o, zostanie wybrany pierwszy pusty
-		for(int i=0; i<chainSize; i++){
-			int n=rand.nextInt(chainSize-1); //Losuj element, który bêdzie zape³niony
-			if (chain[n]>=1){ //Je¿eli wylosowany jest ju¿ zajêty element, spróbuj ponownie (max 10 razy)
-				if (numberTry>10){
+		for(int i=0; i<chainSize*numberChains; i++){
+			int n=rand.nextInt(chainSize); //Losuj element, który bêdzie zape³niony
+			int k=0;
+			if(numberChains>1)
+				k=rand.nextInt(numberChains); //Losuj ³añcuch
+			if (chain[n][k]>=1){ //Je¿eli wylosowany jest ju¿ zajêty element, spróbuj ponownie (max 10 razy)
+				if (numberTry<10){
 					i--;
 					numberTry++;
 					continue;
 				}
 				else{ //Je¿eli nadal nie wylosowano pustego, weŸ pierwszy z brzega
 					for (int j=0; j<n; j++){
-						if (chain[j]==0){
-							n=j;
-							numberTry=0;
-							break;
+						for (int l=0; l<k; l++){
+							if (chain[j][l]==0){
+								n=j;
+								numberTry=0;
+								break;
+							}
 						}
 					}
 				}
 			}
-			chain[n]=2+rand.nextInt(opinions);
+			chain[n][k]=2+rand.nextInt(opinions);
 			//Sprawdzenie otoczenia - czy nastêpuje jakaœ izolacja
 			//Je¿eli opinia jest otoczona przez dwie inne (ale wzajemnie takie same), nastêpuje izolacja
-			if(chainSize-(n+1)>=2){
-				if(chain[n]==chain[n+2]&chain[n+1]!=chain[n]&chain[n+1]!=0){
-					chain[n+1]=1;
-					isolations++;
+			if(numberChains==1){ //Pojedyñczy ³añcuch
+				if(chainSize-(n+1)>=2){
+					if(chain[n][k]==chain[n+2][k]&chain[n+1][k]!=chain[n][k]&chain[n+1][k]!=0){
+						chain[n+1][k]=1;
+						isolations++;
+					}
+				}
+				else if(chainSize-(n+1)<=chainSize-2){
+					if(chain[n][k]==chain[n-2][k]&chain[n-1][k]!=chain[n][k]&chain[n-1][k]!=0){
+						chain[n-1][k]=1;
+						isolations++;
+					}
+				}
+				else if(chainSize-(n+1)<=chainSize-1&chainSize-(n+1)>=1){
+					if(chain[n+1][k]==chain[n-1][k]&chain[n+1][k]!=chain[n][k]){
+						chain[n][k]=1;
+						isolations++;
+					}
 				}
 			}
-			else if(chainSize-(n+1)<=chainSize-2){
-				if(chain[n]==chain[n-2]&chain[n-1]!=chain[n]&chain[n-1]!=0){
-					chain[n-1]=1;
-					isolations++;
+			else{ //N - ³añcuchów 
+				//Przypadek w œrodku ³añcucha
+				if(chainSize-(n+1)>=2&numberChains-(k+1)<=1&numberChains-(k+1)>=numberChains-1){
+					if(chain[n][k]==chain[n+2][k]&chain[n+1][k]!=chain[n][k]&chain[n+1][k]!=0&chain[n+1][k+1]==chain[n+1][k-1]){
+						chain[n+1][k]=1;
+						isolations++;
+					}
 				}
-			}
-			else if(chainSize-(n+1)<=chainSize-1&chainSize-(n+1)>=1){
-				if(chain[n+1]==chain[n-2]&chain[n+1]!=chain[n]){
-					chain[n]=1;
-					isolations++;
+				else if(chainSize-(n+1)<=chainSize-2&numberChains-(k+1)<=1&numberChains-(k+1)>=numberChains-1){
+					if(chain[n][k]==chain[n-2][k]&chain[n-1][k]!=chain[n][k]&chain[n-1][k]!=0&chain[n-1][k+1]==chain[n-1][k-1]){
+						chain[n-1][k]=1;
+						isolations++;
+					}
 				}
+				else if(chainSize-(n+1)<=chainSize-1&chainSize-(n+1)>=1&numberChains-(k+1)<=1&numberChains-(k+1)>=numberChains-1){
+					if(chain[n+1][k]==chain[n-1][k]&chain[n+1][k]!=chain[n][k]&chain[n][k+1]==chain[n][k-1]){
+						chain[n][k]=1;
+						isolations++;
+					}
+				}
+				//Przypadek na granicznych ³añcuchach
+				else if(chainSize-(n+1)>=2&k==0){
+					if(chain[n][k]==chain[n+2][k]&chain[n+1][k]!=chain[n][k]&chain[n+1][k]!=0&chain[n+1][k+1]==chain[n+2][k]){
+						chain[n+1][k]=1;
+						isolations++;
+					}
+				}
+				else if(chainSize-(n+1)>=2&k==numberChains-1){
+					if(chain[n][k]==chain[n+2][k]&chain[n+1][k]!=chain[n][k]&chain[n+1][k]!=0&chain[n+1][k-1]==chain[n+2][k]){
+						chain[n+1][k]=1;
+						isolations++;
+					}
+				}
+				else if(chainSize-(n+1)<=chainSize-2&k==0){
+					if(chain[n][k]==chain[n-2][k]&chain[n-1][k]!=chain[n][k]&chain[n-1][k]!=0&chain[n-1][k+1]==chain[n-2][k]){
+						chain[n-1][k]=1;
+						isolations++;
+					}
+				}
+				else if(chainSize-(n+1)<=chainSize-2&k==numberChains-1){
+					if(chain[n][k]==chain[n-2][k]&chain[n-1][k]!=chain[n][k]&chain[n-1][k]!=0&chain[n-1][k-1]==chain[n-2][k]){
+						chain[n-1][k]=1;
+						isolations++;
+					}
+				}//TODO Niezrobione
+				else if(chainSize-(n+1)<=chainSize-1&chainSize-(n+1)>=1&k==0){
+					if(chain[n+1][k]==chain[n-1][k]&chain[n+1][k]!=chain[n][k]&chain[n][k+1]==chain[n+1][k]){
+						chain[n][k]=1;
+						isolations++;
+					}
+				}
+				else if(chainSize-(n+1)<=chainSize-1&chainSize-(n+1)>=1&k==numberChains-1){
+					if(chain[n+1][k]==chain[n-1][k]&chain[n+1][k]!=chain[n][k]&chain[n][k-1]==chain[n+1][k]){
+						chain[n][k]=1;
+						isolations++;
+					}
+				}
+				//TODO Dodaæ opcje gdy izolacja bêdzie w górnym/dolnym ³añcuchu
+				
 			}
 			isolationsTable[i]=isolations;
+			/**for(int j=0; j<numberChains; j++){
+				for(int l=0; l<chainSize; l++){
+					System.out.print(chain[l][j]);
+				}
+				System.out.println();
+			}
+			System.out.println(i);**/
 		}
 	}
 	
@@ -105,7 +180,7 @@ public class Simulation {
 	}
 	
 	public static void main(String[] args) {
-        Simulation symulacja = new Simulation(1000,2);
+        Simulation symulacja = new Simulation(10000,2,3);
         symulacja.save();
     }
 }
